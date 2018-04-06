6ed69efef26121a3d4179851a11baffc4636000b
Koray Danışma <koray.danisma@gmail.com>
2018-04-07 00:41:46 +0300

    Last Updates

---
+++ index.js
@@ -0,0 +1,44 @@
+var express = require('express');
+
+var app = express();
+
+app.disable('x-powered-by');
+
+var handlebars = require('express-handlebars').create({defaultLayout:'main'})
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 1234);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+	res.render('home');
+});
+
+app.get('/datasets', function(req, res){
+	res.render('datasets');
+});
+
+app.get('/result', function(req, res){
+	var soru = req.query.question;
+
+	var cevap = "Answer of the Question";
+
+	res.render('result', {quest: soru, ans: cevap});
+});
+
+app.get('/about', function(req, res){
+	res.render('about');
+});
+
+app.use(function(req, res){
+	res.type('text.html');
+	res.status(404);
+	res.render('404');
+});
+
+app.listen(app.get('port'), function(){
+	console.log('Express başladı.');
+});
