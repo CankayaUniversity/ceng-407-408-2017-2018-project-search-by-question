11573697ddf4b18b3aaed952d27b1fc061e9f4a6
mhmtsrfglu <memet.serefoglu@gmail.com>
2018-05-12 18:47:25 +0300

    test for process

--- backend_services/test-tools/discovery/discovery_post.php
+++ backend_services/test-tools/discovery/discovery_post.php
@@ -9,10 +9,36 @@
 header("Access-Control-Allow-Origin: *");
 
 require dirname(dirname(__DIR__)) . "/services/Discovery.php";
+require dirname(dirname(__DIR__))."/services/NaturalLanguageUnderstanding.php";
 
 $discovery = new Discovery();
-$keywords = "Tesla";
+$NLU = new NaturalLanguageUnderstanding();
+
+$keywords = "Which NFL team represented the AFC at Super Bowl 50 ?";
 $res = $discovery->query($keywords);
+
+$decodedRes = json_decode($res);
+$arr = [];
+
+foreach ($decodedRes->passages as $item => $val) {
+    if($val->passage_score > 8){
+        array_push($arr,$val->passage_text);
+    }
+}
+$exp = explode("|",$arr[0]);
+
+
 echo "<pre>";
-print_r(json_decode($res));
+//print_r($exp);
 echo "</pre>";
+
+
+
+foreach ($exp as $key => $value) {
+    echo "<pre>";
+    print_r(json_decode($NLU->analyzeGet($value)));
+    echo "</pre>";
+    echo "<br><br><br>";
+    echo "------------------------";
+    echo "<br><br><br>";
+}
